a3f1c40fe9a5c8cfbe8ec9aca6f1275cffaafd03
Devon Wright <35784153+DevonWright@users.noreply.github.com>
2019-11-05 13:30:21 -0800

    Completed question 4.2
    Completed the algorithm for question 4.2, but need to finish Node and BTS classes before I can test it.

--- cci_trees_and_graphs_questions.py
+++ cci_trees_and_graphs_questions.py
@@ -1,3 +1,4 @@
+from math import floor
 """
 4.1) Given a directed graph, design an algorithm to find out whether there is a
      route between two nodes.
@@ -11,3 +12,51 @@ def route(graph, start, destination):
     else:
         return False
 
+"""
+4.2) Given a sorted (increasing order) array with unique integer elements, 
+     write an algorithm to create a binary search tree with minimal height.
+"""
+#TODO: Finish the Node and BTS classes.
+class Node():
+    pass
+
+class Binary_Search_Tree():
+    pass
+
+def create_minimal_tree(array):
+    if array == None:
+        return None
+    
+    #Partition the array 
+    left, midpoint, right = partition(array)
+    
+    #Create a binary search tree with the midpoint being the root
+    tree = Binary_Search_Tree(midpoint)
+    
+    #Create a binary search tree with the left partition and make it the left child of the current tree.
+    tree.root.left_child = create_minimal_tree(left).root
+    
+    #Create a binary search tree with the right partition and make it the left child of the current tree.
+    tree.root.right_child = create_minimal_tree(right).root
+    
+    return tree
+    
+    
+def partition(array):
+    if len(array) == 1:
+        #There is no left of right sides, just the midpoint.
+        return None, array[0], None
+    elif len(array) == 2:
+        #There is no right side, just the midpoint and the left side.
+        return array[0], array[1], None
+    else:
+        #Find the middle index
+        middle_index = floor(len(array)/2) 
+        
+        #Partition the array into 3 parts
+        left_side = array[:middle_index]
+        right_side = array[middle_index + 1:]
+        middle_element = array[middle_index]
+        
+        return left_side, middle_element, right_side
+    
